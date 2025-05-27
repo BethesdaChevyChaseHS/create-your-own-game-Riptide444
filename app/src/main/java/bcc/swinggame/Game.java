88e06ed123efc612f833f52168dfd34c63ebd15b
Daniel Skendaj <68509946+Riptide444@users.noreply.github.com>
2025-05-27 17:42:20 -0400

    Create Game.java

---
+++ app/src/main/java/bcc/swinggame/Game.java
@@ -0,0 +1,31 @@
+import java.awt.Color;
+
+public class Game {
+    private Color currentPlayer;
+    private boolean gameOver;
+
+    public Game() {
+        currentPlayer = Color.RED;
+        gameOver = false;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void nextPlayer() {
+        if(currentPlayer == Color.RED) {
+            currentPlayer = Color.YELLOW;
+        } else {
+            currentPlayer = Color.RED;
+        }
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public void gameOver() {
+        gameOver = true;
+    }
+}
